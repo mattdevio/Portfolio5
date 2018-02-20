f9062924b465d49f1e91ef1630eb5269bb30eced
Matt Greenberg <magreenberg@fullsail.edu>
2018-02-19 23:40:28 -0700

    Signup for is working and is styled. I just need a route for the new user to go to

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import {
   withRouter,
 } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 import { auth, db } from '../../firebase'
 import * as routes from '../../constants/routes'
@@ -19,7 +20,7 @@ const SignUpPage = ({ history }) => (
   </div>
 )
 
-const FormInput = (props) => {
+const FormInput = (props, { bubbleState }) => {
   const {
     propertyName,
     value,
@@ -27,19 +28,25 @@ const FormInput = (props) => {
     placeHolder,
     label,
   } = props
+
   return (
     <Fragment>
       <label htmlFor={propertyName} className={styles.label}>{label}:</label>
       <input
         value={value}
-        onChange={event => this.setState(byPropKey(propertyName, event.target.value))}
+        onChange={event => bubbleState(propertyName, event.target.value)}
         type={type}
         placeholder={placeHolder}
         id={propertyName}
         className={styles.input}
+        autoComplete='off'
       />
     </Fragment>
   )
+}
+
+FormInput.contextTypes = {
+  bubbleState: PropTypes.func,
 }
 
 const INITIAL_STATE = {
@@ -57,16 +64,41 @@ class SignUpForm extends Component {
     this.onSubmit = this.onSubmit.bind(this)
   }
 
+  getChildContext() {
+    return {
+      bubbleState: (propertyName, value) => {
+        this.setState(byPropKey(propertyName, value))
+      },
+    }
+  }
+
   onSubmit(event) {
     const {
       displayName,
       email,
       passwordOne,
+      passwordTwo,
     } = this.state
 
     const {
       history,
     } = this.props
+
+    if (displayName.trim().length < 3) {
+      this.setState({
+        error: { message: 'The display name is too short.' },
+      })
+      return
+    }
+
+    if (passwordOne !== passwordTwo) {
+      this.setState({
+        error: { message: 'The passwords don\'t match.' },
+      })
+      return
+    }
+
+    // password length and email are validated by firebase
 
     auth.doCreateUserWithEmailAndPassword(email, passwordOne)
       .then((authUser) => {
@@ -129,13 +161,19 @@ class SignUpForm extends Component {
           error &&
           <p className={styles.error}>Error: {error.message}</p>
         }
-        <button type='submit'>
-          Sign Up
-        </button>
+        <div className={styles.actions}>
+          <button type='submit' className={styles.signup}>
+            Sign Up
+          </button>
+        </div>
       </form>
     )
   }
 
 } // end SignUpForm class
+
+SignUpForm.childContextTypes = {
+  bubbleState: PropTypes.func,
+}
 
 export default withRouter(SignUpPage)
