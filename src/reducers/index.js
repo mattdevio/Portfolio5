c64bd5f806b5b69f722ef02a70c10cf525e2eeab
Matt Greenberg <magreenberg@fullsail.edu>
2018-02-22 01:23:27 -0700

    added budget reducer to the redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import sessionReducer from './session'
 import userReducer from './user'
+import budgetReducer from './budget'
 
 const rootReducer = combineReducers({
   sessionState: sessionReducer,
   userState: userReducer,
+  budgetState: budgetReducer,
 })
 
 export default rootReducer
