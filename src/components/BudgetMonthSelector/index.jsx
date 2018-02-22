300058f473449e4d6e81ae5eb403c16ce01c67e7
Matt Greenberg <magreenberg@fullsail.edu>
2018-02-21 23:57:22 -0700

    Visual buttons now display on the year - month selector

--- src/components/BudgetMonthSelector/index.jsx
+++ src/components/BudgetMonthSelector/index.jsx
@@ -1,20 +1,46 @@
 import React, { Component } from 'react'
+import moment from 'moment'
 
 import styles from './BudgetMonthSelector.css'
 
 class BudgetMonthSelector extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentMonth: moment().format('MMMM'),
+      currentYear: moment().format('YYYY'),
+      monthsShort: moment.monthsShort(),
+    }
+  }
+
   componentDidMount() {
-    console.log('Budget Month Selector Mounted')
+    console.log('cdm')
   }
 
   render() {
-    const month = 'January'
-    const year = '2018'
+    const {
+      currentMonth,
+      currentYear,
+      monthsShort,
+    } = this.state
+
     return (
       <div className={styles.container}>
-        <h1 className={styles.month}>{month}<span>{year}</span></h1>
+        <div className={styles.topbar}>
+          <h1 className={styles.month}>{currentMonth}<span>{currentYear}</span></h1>
+        </div>
         <div className={styles.select}>
-          <div>Month</div>
+          {monthsShort.map((month) => {
+            let s = styles.monthBtn
+            const btnMonth = moment(month, 'MMM').format('MMMM')
+            s += (btnMonth === currentMonth) ? ` ${styles.active}` : ''
+            return (
+              <div className={s} key={month}>
+                <span>{month}</span>
+              </div>
+            )
+          })}
         </div>
       </div>
     )
